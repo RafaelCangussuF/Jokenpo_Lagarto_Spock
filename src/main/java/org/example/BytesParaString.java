ee39619973d3e918afa42bf5508298cb06819a5f
RafaelCangussuF <rafaelcangussuf@gmail.com>
2022-12-03 23:31:02 -0300

    Initial commit

---
+++ src/main/java/org/example/BytesParaString.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.io.UnsupportedEncodingException;
+
+
+public class BytesParaString {
+
+        public static void main(String[] args) throws UnsupportedEncodingException {
+
+            byte[] bytes = new byte[]{116, 101, 120, 116, 111, 32, 116, 101, 115, 116, 101};
+
+            String texto = new String(bytes, "UTF-8");
+
+            System.out.println(texto);
+
+        }
+}
